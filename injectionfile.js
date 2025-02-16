56db7c8f22bd6304e85185511de4edd6c1011add
Quema <myoon0929@gmail.com>
2025-02-16 19:22:02 +0900

    fix comment

--- injectionfile.js
+++ injectionfile.js
@@ -4,10 +4,10 @@ const originalCreateElement = document.createElement;
 
 document.createElement = function (message) {
     const element = originalCreateElement.apply(this, arguments);
-    if (message == 'video' || message == 'audio') {
-        console.log('Video or audio element created:', element);
+    if (message === 'video' || message === 'audio') {
+        console.log('🎥 Video or audio element created:', element);
         videoElements.push(element);
-        console.log('Video elements:', videoElements);
+        console.log('🎥 Video elements:', videoElements);
     }
     return element;
 };
