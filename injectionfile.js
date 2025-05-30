b20ad701887c6856ddcdded0e73a5bae274beedb
Quema <myoon0929@gmail.com>
2025-05-30 20:06:14 +0900

    fix code style

--- injectionfile.js
+++ injectionfile.js
@@ -1,9 +1,9 @@
 let videoElements = [];
 
-const originalCreateElement = document.createElement;
+const originalCreateElement = document.constructor.prototype.createElement;
 
 document.createElement = function (message) {
-    const element = originalCreateElement.apply(this, arguments);
+    const element = originalCreateElement.call(document, message); 
     if (message === 'video' || message === 'audio') {
         console.log('🎥 Video or audio element created:', element);
         videoElements.push(element);
